ca80487884fda2d62ca55caf001827bb4fe676de
AyGwair <58555778+AyGwair@users.noreply.github.com>
2020-04-28 08:59:22 +0200

    Add files via upload

--- vues/v_pied.php
+++ vues/v_pied.php
@@ -1,6 +1,8 @@
-<body>
-
-<?php
-    
-?>
-</body>
+<div id="pied">
+    <?php
+    if (isset($_SESSION['id'])) {
+    	echo 'Utilisateur '.$_SESSION['pseudo']; 
+    	echo ' connecte depuis '.$_SESSION['heureConnexion']; 
+    }
+    ?>
+</div>
