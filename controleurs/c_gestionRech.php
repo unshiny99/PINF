4dcba9fe95988615be7c53efb90de61af86b55e8
Maxime Frémeaux <49973150+unshiny99@users.noreply.github.com>
2020-04-28 17:07:04 +0200

    Update c_gestionRech.php

--- controleurs/c_gestionRech.php
+++ controleurs/c_gestionRech.php
@@ -1,4 +1,18 @@
 <?php
+
+$cherche = valider("debutNom");
+if($cherche) {
+	$resultat=array();
+	$retour = array();
+	$sql= "SELECT * FROM commerce WHERE nom_commerce LIKE \"$cherche\"";
+	$resultat=parcoursRs(SQLSelect($sql));
+
+	$retour["resultat"] = $resultat;
+	$retour["recherche"] = $cherche;
+	echo json_encode($retour);
+	die("");
+}
+
 $action = $_REQUEST['action'];
 switch($action)
 {
