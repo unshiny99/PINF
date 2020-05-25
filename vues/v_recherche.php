503d7980f6c71b2cdcba5745bf4beeab6b903849
Azeyeye <47383556+Azeyeye@users.noreply.github.com>
2020-05-25 16:53:10 +0200

    Update v_recherche.php
    ajout de l'iframe

--- vues/v_recherche.php
+++ vues/v_recherche.php
@@ -3,7 +3,7 @@
 	<?php
 		foreach($nom_commerce as $commerce)
 		{
-			?> <fieldset> 
+			?> <fieldset>
 			<h1> Les Informations Commerce </h1> <?php
 			echo 'Nom commerce : '.$commerce['nom_commerce'];
 			?></br><?php
@@ -16,8 +16,12 @@
 			echo 'code postal : '.$commerce['code_postal'];
 			?></br><?php
 			echo 'adresse : '.$commerce['adresse'];
-			?></br><?php
-			?> </fieldset> <?php
+			?></br><?php $src = "https://maps.google.com/maps?width=50%&amp;height=300&amp;hl=en&amp;q=France%2C". $commerce['adresse']."+(mon%20site)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed";
+			//echo $src;
+			?> </br>
+			<div style="width: 50%"><iframe width="50%" height="300" src="<?php echo $src; ?>" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/draw-radius-circle-map/">Create radius map</a></iframe></div><br /><?php
+			?>
+		</fieldset> <?php
 		}
 	?>
 </div>
